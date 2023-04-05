8903d7685c8bbb6d96c32be71fb3725cd8b9bdd0
Raul Soares - Fedora <raulrodriguessoares@gmail.com>
2023-04-05 11:20:28 -0300

    feat: neonbutton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,71 @@
 import * as icons from "../icons/icons";
 import { Link } from "react-router-dom";
-import './Header.css'
+import "./Header.css";
+import "./NeonButton.css";
 import Foto from "../img/logo.png";
 import { useState } from "react";
 
 export default function Header() {
-    const [mostrar, setMostrar] = useState("none")
-    const [tamanhoTela, setTamanhoTela] = useState(document.body.clientWidth)
-    
-    window.addEventListener("resize", () => {
-        setTamanhoTela(document.body.clientWidth)
-    })
-    
-    function menu() {
-        setMostrar(mostrar === "none" ? "block" : "none")
-        console.log(document.body.clientWidth)
-    }
-    return (
+  const [mostrar, setMostrar] = useState("none");
+  const [tamanhoTela, setTamanhoTela] = useState(document.body.clientWidth);
 
-<div className="coitainer-header">
-        <header>
+  window.addEventListener("resize", () => {
+    setTamanhoTela(document.body.clientWidth);
+  });
 
-            <div className="foto">
-            <img src={Foto} className="logo" alt="logo" />
-            <p className="nome">Murilo Marcondes Silva</p>
-            </div>
-
-            <div className="filtro" style={{display: tamanhoTela > 700 ? "block" : mostrar}}>
-                <ul>
-                    <li>
-                        <Link className="icon" to='/' >
-                        Sobre Mim <icons.sobre />
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link className="icon" to='/contatos' >
-                        Contatos <icons.Telegram />
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link className="icon" to='/certificados'>
-                        Certificados <icons.list />
-                        </Link>
-                    </li>
-                
-                </ul>
-            </div>
-            
-            <button className="button-menu" onClick={menu}>Menu</button>
-        </header>
-
-        
-        
-</div>
-
-    );
+  function menu() {
+    setMostrar(mostrar === "none" ? "block" : "none");
+    console.log(document.body.clientWidth);
   }
+  return (
+    <div className="coitainer-header">
+      <header>
+        <div className="foto">
+          <img src={Foto} className="logo" alt="logo" />
+          <p className="nome">Murilo Marcondes Silva</p>
+        </div>
+
+        <div
+          className="filtro"
+          style={{ display: tamanhoTela > 700 ? "block" : mostrar }}
+        >
+          <ul>
+            <li>
+              <Link className="icon" to="/">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Sobre Mim <icons.sobre />
+              </Link>
+            </li>
+
+            <li>
+              <Link className="icon" to="/contatos">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Contatos <icons.Telegram />
+              </Link>
+            </li>
+
+            <li>
+              <Link className="icon" to="/certificados">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Certificados <icons.list />
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <button className="button-menu" onClick={menu}>
+          Menu
+        </button>
+      </header>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Routes, Navigate,} from "react-router-dom";
-import './index.css';
-import Home from './pages/sobre/home';
-import Contato from './pages/contatos/Contatos';
-import Certificados from './pages/certificados/Certificados';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/sobre/home";
+import Contato from "./pages/contatos/Contatos";
+import Certificados from "./pages/certificados/Certificados";
+import reportWebVitals from "./reportWebVitals";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-    <Router basename='/'>
+    <Router basename="/">
       <Routes>
-        <Route path='/' index={true} element={<Home />} />
-        <Route path='/Contatos' children={true} element={<Contato  />}/> 
-        <Route path='/Certificados' children={true} element={<Certificados  />}/>
-        <Route path='/notFound' element={<noFound  />}/>
-        <Route path='*' errorElement={true} element={<Navigate to='/notFound'  />}/>
+        <Route path="/" index={true} element={<Home />} />
+        <Route path="/Contatos" children={true} element={<Contato />} />
+        <Route
+          path="/Certificados"
+          children={true}
+          element={<Certificados />}
+        />
+        <Route path="/notFound" element={<noFound />} />
+        <Route
+          path="*"
+          errorElement={true}
+          element={<Navigate to="/notFound" />}
+        />
       </Routes>
     </Router>
   </React.StrictMode>
