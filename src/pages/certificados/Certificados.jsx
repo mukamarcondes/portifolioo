17e76bb132d1a2f7b7cf8108ae4978f9f48840b5
mukamarcondes <mukasilva26@gmail.com>
2023-05-10 10:59:44 -0300

    acabando

--- src/pages/certificados/Certificados.jsx
+++ src/pages/certificados/Certificados.jsx
@@ -6,43 +6,28 @@ function Certificados() {
   return (
     <div className="container-geral">
       <Header />
-      <div class="containeer">
-        <div class="carousel">
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-          <div class="carousel__face">
-            <span></span>
-          </div>
-        </div>
+
+  <div className="oi">
+
+      <div class="pic-ctn">
+          <img className="pic" src="https://i.pinimg.com/originals/71/ee/32/71ee32577432648f9e45fbd63b2cf261.jpg" alt="" />
+          <img className="pic" src="https://global-uploads.webflow.com/5babd501fb0eee7ea33c30a3/6058fa7293d8030bd31bdf7b_-5g.jpg" alt="" />
+          <img className="pic" src="https://www.globalmind.com.br/wp-content/uploads/2021/05/aws-header_tvMEUF2.width-800.png" alt="" />
+          <img className="pic" src="https://dkrn4sk0rn31v.cloudfront.net/uploads/2018/03/01133925/javascript.png" alt="" />
+          <img className="pic" src="https://www.piramidal.com.br/blog/wp-content/uploads/2022/01/shutterstock_1919518907.png" alt="" /> 
       </div>
+
+      <div class="pic-ctn">
+          <img className="pic" src="http://arquivo.devmedia.com.br/marketing/img/guia-html-38051.png" alt="" />
+          <img className="pic" src="https://www.blogson.com.br/wp-content/uploads/2021/03/csharp1-738x430.jpg" alt="" />
+          <img className="pic" src="https://gtrigueiro.com.br/wp-content/uploads/2021/09/lgpd-vanzolini-1200x874-1-1024x746.jpg" alt="" />
+          <img className="pic" src="https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2022/10/linux-6-0-e-oficialmente-lancado-veja-o-que-ha-de-novo.jpg" alt="" />
+          <img className="pic" src="https://d1i4tvf70h7zdy.cloudfront.net/ead/cursos/5c782f2ad62ee.png" alt="" />
+          <img className="pic" src="https://apexensino.com.br/wp-content/uploads/2020/05/python-1280x640.jpg" alt="" />
+          <img className="pic" src="https://www.lockheedmartin.com/content/dam/lockheed-martin/eo/photo/suppliers/cybersecurity-masthead-1920.jpg.pc-adaptive.full.medium.jpeg" alt="" />
+      </div>
+  </div>
+     
       <div className="escrita">
         <legend className="curso">CURSOS EXTRASCURRICULARES</legend>
 
